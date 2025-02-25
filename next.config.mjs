c49dd5f8b67c5a43908d0ec9ecfc451b5ecf61e4
Pavel <u6430176@au.edu>
2025-02-25 16:32:46 +0700

    Disable turbo mode in experimental settings of Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turboMode: false,
+  },
   images: {
     domains: [
       'knpos-bucket-senior-project-2.s3.us-east-1.amazonaws.com',
